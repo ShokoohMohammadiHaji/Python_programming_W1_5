9e9074caa2223473c3cb39283f291a40ccbf1f5c
shokooh <shokooh.mohammadi@gmail.com>
2026-09-20 11:33:22 +0300

    W1_5

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Calculate the area of a wall.")
+Feed = input("Enter the width in meters: ")
+Width = int(Feed)
+Feed = input("Enter the height in meters: ")
+Height = int(Feed)
+print(f"Width is {Width} m and height is {Height} m.")
+Area = Width * Height
+print(f"The wall will be {Area} square meters.")
+
+# to solve decimal problem use float instead of int to convert the inputs.
